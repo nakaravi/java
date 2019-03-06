dcc1e368d6f9af658c4560fdea046b2b965c4084
N. Ravikumar <ravidata@gmail.com>
2019-03-06 17:35:10 +0530

    Create mysql1.js

---
+++ nodejs/mysql1.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({ 
+	host:"localhost",
+	user: "root", 
+	password: "", 
+	database: "api"
+});
+ 
+connection.query('SELECT * from products', function (error, results, fields) {
+  if (error) throw error;
+ console.log(results);
+});
