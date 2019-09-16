31c86b96b3b5bcf304a5b444e80b162a4325bac6
N. Ravikumar <ravidata@gmail.com>
2019-09-16 17:46:34 +0530

    Update mproducts.js

--- nodejs/server/routes/mproducts.js
+++ nodejs/server/routes/mproducts.js
@@ -1,6 +1,7 @@
 //const express = require('express');
 const router = express.Router();
-var Db = require('mongodb').Db,
+bson = require('../../node_modules/bson/browser_build/bson');
+/*var Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
     ReplSetServers = require('mongodb').ReplSetServers,
@@ -9,52 +10,73 @@ var Db = require('mongodb').Db,
     GridStore = require('mongodb').GridStore,
     Grid = require('mongodb').Grid,
     Code = require('mongodb').Code,
-    assert = require('assert');
+    assert = require('assert');*/
 
 //var connection = mysql.createConnection('mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
 
+	//var mongoclient = new MongoClient(new Server("localhost", 27017), {native_parser: true});
+
+var mc = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
 
 //get all records
 router.get('/', (req, res) => {
+	var  ary = [];
 
-	var db = new Db('api', new Server('localhost', 27017));
-		
 	
-	db.collections('products', (err, collection)=>{
-		
-		/*collection.insertOne({id:3,name:'DELL',description:'Dell products'}, function(err, result) {
-		console.log('aaaaaaaaaaaaaaa',err, result);
-		});*/
-		collection.find(1,1,true).toArray((err, items)=>{
-			console.log(items.length);
-				if(err) console.log(err);
-				console.log(items);
-				res.json({result:'success'});
-			});
-		});
 	
-	//res.json({result:'success'});
-	/*
-	const Test = mongoCon.collection('products');
-	console.log(Test);
+
+	mc.connect(url, function(err, db) {
+		if (err) throw err;
 	
-	/*.then((err, db)=>{
+		var dbo = db.db("api");
+		dbo.collection("products").find().toArray((err, items)=>{
+			
+			if(err){res.json({result:'failure', data:[]});}
+			
+			items.forEach(item=>{
+				
+				ary.push(item);
+			})
+			res.json({result:'success', data:JSON.parse(JSON.stringify(ary))});
 		
-		db.collection('products', (err, collection)=>{
-			collection.find().toArray((err, items)=>{
-				if(err) throw err;
-				console.log(items);
-				res.json(items);
-			});
-		});
-	});*/
+		})
+	
+	});
+	
 	
 });
+router.get('/:search',(req, res)=>{
+	console.log('inside', req.params.id);
+	let txt = req.params.search;
+	var ary = [];
+	mc.connect(url, (err, db)=>{
+		var q = {name:txt}
+		//var query = { name: /^S/ };
+		//db.users.find({"name": /.*m.*/})
+		//or, similar:
+		//db.users.find({"name": /m/})
+		db.db("api").collection("products").find(q).toArray((err, items)=>{
+			
+			if(err){res.json({result:'failure', data:[]});}
+			
+			items.forEach(item=>{
+				
+				ary.push(item);
+			})
+			res.json({result:'success', data:JSON.parse(JSON.stringify(ary))});
+		
+		})
+
+	})
+});
+
 
 //get single records using parameter.
 router.get('/:id',(req, res)=>{
 	console.log('inside', req.params.id);
 	let id = req.params.id;
+	var ary = [];
 	connection.query('SELECT * from products where id='+id, function (error, results, fields) {
 		if (error) throw error;
 		var resultJson = JSON.stringify(results);
@@ -74,23 +96,30 @@ router.get('/:id',(req, res)=>{
 //post records and insert to db
 router.post('/',(req, res)=>{
 	//console.log('post content.................',req);
-	let qry = 'insert into products (name, description, price, category_id) values ("' + req.body.name +'","'+ req.body.description+'","'+ req.body.price+'","1")';
-	console.log(qry);
-	connection.query(qry, (error, results, fields) => {
-		if(error) throw error;
-		var resultJson = JSON.stringify(results);
-		var apiResult = {
-			result:'success'
-		};
-		
-		apiResult.data = resultJson;
-				
-		res.json(apiResult);
+	let qry = 'insert into products (name, description) values ("' + req.body.name +'","'+ req.body.description+'")';
+	let json = {name:req.body.name, description:req.body.description};
+	mc.connect(url, (err, db)=>{
+
+		db.db("api").collection("products").insert(json,(err, data)=>{
+			console.log(data)
+		})
+		res.json({result:"success"});
+	});
+});
+//update records
+router.patch('/:id', function(req, res) { 
+	let json = {name:req.body.name, description:req.body.description};
+	var myquery = { name: "HP" };
+	var newvalues = { $set: {name: "Mickey", description: "Canyon 123" } };
+	mc.connect(url, (err, db)=>{
+
+		db.db("api").collection("products").updateOne(myquery, newvalues,(err, data)=>{
+			console.log(data)
+		})
+		res.json({result:"success"});
 	});
 	
 });
-//update records
-router.patch('/:id', function(req, res) { });
 
 //delete record using id
 router.delete('/:id',(req, res)=>{
