d54f4bff53e53990b2c2f31649cb56127607000f
N. Ravikumar <ravidata@gmail.com>
2019-09-17 15:42:33 +0530

    Create config.js

---
+++ nodejs/config.js
@@ -0,0 +1,9 @@
+var databaseOptions = {
+    host     : 'localhost',
+    database : 'api',
+    user     : 'root',
+    password : '',
+    port     : '3306'
+};
+module.exports = {databaseOptions: databaseOptions} ;
+
